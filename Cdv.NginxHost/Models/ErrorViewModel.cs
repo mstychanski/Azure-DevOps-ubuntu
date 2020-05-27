5b8790905f968e3c7f739ad01868746928e9c763
aziemianski <artzie92@gmail.com>
2020-05-27 08:43:16 +0200

    Intiial commit

---
+++ Cdv.NginxHost/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cdv.NginxHost.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
